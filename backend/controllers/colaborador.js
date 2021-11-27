a4fb36f8936e8153a72da88da1cf5089b5956a39
Rafael <rafaelsfsoares@gmail.com>
2021-11-27 18:44:49 -0300

    FEAT: show/save controller criado

--- backend/controllers/colaborador.js
+++ backend/controllers/colaborador.js
@@ -13,7 +13,31 @@ class colaboradorController {
      
     } 
 
+    static async show (req, res) {
+        const {id} = req.params
+        try {
+            const idColaboradores = await database.Colaborador.findOne( { where:
+                 { id: Number(id) }
+                })
+            return res.status(200).json(idColaboradores);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
 
 
+    static async save (req, res) {
+        const newColaboradores =  req.body
+        try {
+            const createColaboradores = await database.Colaborador.create(newColaboradores);
+            return res.status(200).json(createColaboradores);
 
+        } catch (error) {
+          return res.status(500).json(error.message);
+
+        }
+    } 
 }
+
+
+module.exports = colaboradorController;
