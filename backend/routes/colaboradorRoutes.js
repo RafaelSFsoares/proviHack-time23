8df59179dfdb434caa4dfcd4ad66794f29d7538e
Helio Franco <dev.francohel@gmail.com>
2021-11-27 19:57:29 -0300

    FEAT: put/colaborador:id.update criado

--- backend/routes/colaboradorRoutes.js
+++ backend/routes/colaboradorRoutes.js
@@ -6,7 +6,7 @@ const router = Router();
 router.get('/colaborador', colaboradorController.index);
 router.get('/colaborador:id', colaboradorController.show);
 router.post('/colaborador', colaboradorController.save);
-router.put('/:id', colaboradorController.update);
+router.put('/colaborador:id', colaboradorController.update);
 router.delete('/:id', colaboradorController.remove);
 
 
