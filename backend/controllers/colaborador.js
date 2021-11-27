7860a0f883d38040bdce977b12eb9bdcdb9bc6aa
Helio Franco <dev.francohel@gmail.com>
2021-11-27 17:37:47 -0300

    FEAT: update colaborador criado

--- backend/controllers/colaborador.js
+++ backend/controllers/colaborador.js
@@ -11,9 +11,30 @@ class colaboradorController {
             return res.status(500).json(error.message);
         }
      
+
+        
     } 
 
+    static async update(req, res){
+        const {id} = req.params
+        const newInfo = req.body
+        try{
+            await database.Colaborador.update(newInfo, { 
+                where: {
+                id: Number(id)
+            }     
+            })
+            const updateColaborador = await database.Colaborador.findOne({
+                where: {
+                    id:Number(id)
+                }
+            })
+            return res.status(200).json(updateColaborador)
 
+        }catch(error){
+            return res.status(500).json(error.message)
+        }
+    }
 
 
 }
