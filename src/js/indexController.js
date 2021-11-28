e53e26aab9e7ed8760acc567459e58ba8f1a69ce
Helio Franco <dev.francohel@gmail.com>
2021-11-28 02:27:52 -0300

    FEAT: direcionamento do botao colaboradores criado

--- src/js/indexController.js
+++ src/js/indexController.js
@@ -2,3 +2,8 @@ let cadastro = document.getElementById('btnEntrar')
 cadastro.addEventListener('click',()=>{
     return location = './src/html/login.html'
 })
+
+let colabora = document.getElementById('btnColaborador')
+colabora.addEventListener('click', ()=>{
+    return location = './src/html/cadastro.html'
+})
