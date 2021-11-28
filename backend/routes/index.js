3d52dad18628b0365c9ebb7af08aadb33f204edd
Rafael <rafaelsfsoares@gmail.com>
2021-11-28 01:53:24 -0300

    FEAT: adicionando CORS

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,18 @@
 const colaboradores = require('./colaboradorRoutes');
 const express = require('express');
+const cors = require('cors');
 
 module.exports = app => {
+    app.use(cors())
+    app.use(express.json());
+    
     app.use((req, res, next) => {
         console.log(req.headers.host, new Date().toLocaleTimeString());
         next();
     });
 
-    app.use(express.json());
-    app.use(colaboradores)
+    app.use(colaboradores);
+    
+    
     
 }
