ec3ac4f6801e635f129459e16672a0f771754eb5
Rafael <rafaelsfsoares@gmail.com>
2021-11-28 11:13:20 -0300

    FEAT: controller palavras.js criado

---
+++ backend/controllers/palavras.js
@@ -0,0 +1,80 @@
+const database = require('../models');
+
+
+class palavrasController {
+    static async index(req, res) {
+        try {
+         const palavrasColocadas = await database.Palavras.findAll()
+         return res.status(200).json(palavrasColocadas);
+            
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+     
+
+        
+    } 
+
+    static async show (req, res) {
+        const {id} = req.params
+        try {
+            const idPalavras = await database.Palavras.findOne( { where:
+                 { id: Number(id) }
+                })
+            return res.status(200).json(idPalavras);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+
+    static async save (req, res) {
+        const novasPalavras =  req.body
+        try {
+            const createPalavras = await database.Palavras.create(novasPalavras);
+            return res.status(200).json(createPalavras);
+
+        } catch (error) {
+          return res.status(500).json(error.message);
+
+        }
+    } 
+
+    static async update(req, res){
+        const {id} = req.params
+        const newInfo = req.body
+        try{
+            await database.Palavras.update(newInfo, { 
+                where: {
+                id: Number(id)
+            }     
+            })
+            const updatePalavra = await database.Palavras.findOne({
+                where: {
+                    id:Number(id)
+                }
+            })
+            return res.status(200).json(updatePalavra)
+
+        }catch(error){
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async remove(req, res){
+        const {id} = req.params
+        try{
+            await database.Palavras.destroy({
+                where: {
+                    id:Number(id)
+                }
+            })
+            return res.status(200).json({mensagem: `id ${id} deletado`})
+
+        }catch(error){
+            return res.status(500).json(error.message)
+        }
+    }
+}
+
+module.exports = palavrasController;
