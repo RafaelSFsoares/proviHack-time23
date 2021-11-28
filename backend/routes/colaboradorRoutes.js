0e4cc48232a68f84823e1b713e20d7223735dc24
Rafael <rafaelsfsoares@gmail.com>
2021-11-28 11:20:38 -0300

    FIX: ajeitando rotas

--- backend/routes/colaboradorRoutes.js
+++ backend/routes/colaboradorRoutes.js
@@ -4,10 +4,10 @@ const colaboradorController = require ('../controllers/colaborador.js');
 const router = Router();
 
 router.get('/colaborador', colaboradorController.index);
-router.get('/colaborador:id', colaboradorController.show);
+router.get('/colaborador/:id', colaboradorController.show);
 router.post('/colaborador', colaboradorController.save);
-router.put('/colaborador:id', colaboradorController.update);
-router.delete('/colaborador:id', colaboradorController.remove);
+router.put('/colaborador/:id', colaboradorController.update);
+router.delete('/colaborador/:id', colaboradorController.remove);
 
 
 
