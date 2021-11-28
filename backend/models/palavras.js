687cab880ea0312473bfa049e5140ebb72f40313
Rafael <rafaelsfsoares@gmail.com>
2021-11-28 11:15:01 -0300

    FEAT: palavras.js adicionado/criado no models

---
+++ backend/models/palavras.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Palavras extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Palavras.init({
+    lingua: DataTypes.STRING,
+    palavra_nativa: DataTypes.STRING,
+    palavra_portugues: DataTypes.STRING,
+    transcricao: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Palavras',
+  });
+  return Palavras;
+};
