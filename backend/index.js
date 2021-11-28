c741c093bf97b79e971f3788472ce0c19df97c95
Rafael <rafaelsfsoares@gmail.com>
2021-11-28 01:49:43 -0300

    FEAT: adicionando caminho routes

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 const express = require ('express');
-
+const routes = require('./routes')
 const app = express()
 const port = 3000;
 
-// routes(app)
+routes(app)
 
 
 app.listen(port, () => console.log(`Listening ${port}`));
