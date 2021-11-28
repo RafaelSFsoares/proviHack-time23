1d90eb816585a68a5e6b8635cab303097dd0d140
Rafael <rafaelsfsoares@gmail.com>
2021-11-28 16:33:07 -0300

    FIX: Solucionando bug na requisiição

--- src/js/buscaPalavras.js
+++ src/js/buscaPalavras.js
@@ -4,5 +4,29 @@ async function buscaPalavras(lingua) {
     const url = `http://localhost:3000/palavras/${lingua}`
     let apiResponse = await fetch(url).then(response => response.json());
     console.log(apiResponse);
+    let caixa = document.getElementById('caixa');
+
+    for (let i = 0; i < apiResponse.length; i++) {
+        
+        var div = document.createElement('div');
+        caixa.append(div);
+        div.setAttribute('class','palavra')
+
+        div.innerHTML = `<h1 id="teste">${apiResponse[i].lingua}</h1>`
+
+        // for (let j = 0; j < apiResponse.length; j++) {
+
+        //     var paragrafo = document.createElement('p');
+
+        //     div.appendChild(paragrafo);
+
+        //     paragrafo.innerHTML = apiResponse[i].lingua
+            
+        // }
+        
+        
+    }
+
+    
     
 }
